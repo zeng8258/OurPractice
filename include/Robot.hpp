c2c602c90e0683c322e4e45c67ab0fdaadc2e638
suolangcidan <15628591588@139.com>
2024-07-16 17:32:56 +0800

    更改了robot的hpp 文件与cpp文件

--- include/Robot.hpp
+++ include/Robot.hpp
@@ -12,49 +12,30 @@ using json = nlohmann::json;
 
 class Robot
 {
-public:
+  public:
     // 构造和析构
-    Robot(): _name("Robot"), _key(KEY) 
-    {_response=R"(
-      {
-        "model": "gpt-3.5-turbo",
-        "messages": [{"role": "user", "content": ""}],
-        "max_tokens": 4096,
-        "temperature": 1
-      }
-      )"_json;
-    }
+    Robot()_name("Robot"), _key(KEY){}
+    Robot(std::string name, std::string key);
     Robot(const Robot&);
     ~Robot();
 
     // 功能
     //给AI命名
-    void setName(const std::string& name) { this->_name = name; }
-        /**
-     * 获取名称
-     * 这个成员函数允许获取当前对象的名称。它返回一个 const std::string& 类型的引用，
-     * 指向对象内部存储的名称字符串。获得的引用可以用来访问，但不能修改字符串内容，
-     * 确保了对象状态的一致性。
-     *
-     * 返回值：
-     * const std::string& - 当前对象的名称的常量引用。
-     */
-    const std::string& getName() const { return this->_name; }
-     /**
-     * 处理用户输入的函数
-     * 这个函数用于接收用户的输入，并将其存储到对象的_response成员变量中。具体来说，它将用户输入的字符串赋值给_response对象中的一个特定键值对：["messages"][0]["content"]。这个键值对通常用于表示一个消息列表中的第一条消息的内容。通过这种方式，对象可以记录下用户的输入，以便后续处理或分析。
-     * @param input 用户输入的字符串。这个字符串将被存储在_response成员变量中的["messages"][0]["content"]键对应的值中。
-     */   
-    void ueserInput(const std::string& input)
-    {
-        this->_response["messages"][0]["content"] = input;
-        auto completion = openai::chat().create(_response);
-        std::cout<<_name<<':'<< completion.dump(2) <<std::endl;
-    }
-
-private:
+    void setName(const std::string& name);
+    //返回AI的名字   
+    const std::string& getName();
+    //返回用户的输入
+    const std::string& getInput();
+    //用户输入
+    void ueserInput(const std::string& input);
+    //AI回复
+    void response();
+    //更改对话温度
+    void setTemperature(double  temperature);
+  private:
     std::string _name;//AI的名字
     char *_key;//密钥
+    std::string _input;//用户输入的内容
     json _response;//AI的回复
 };
 
--- src/Robot.cpp
+++ src/Robot.cpp
@@ -1,1 +1,58 @@
 #include "Robot.hpp"
+
+Robot::Robot()_name("Robot"), _key(KEY) 
+    {_response=R"(
+      {
+        "model": "gpt-3.5-turbo",
+        "messages": [{"role": "user", "content": ""}],
+        "max_tokens": 4096,
+        "temperature": 0
+      }
+      )"_json;
+    }
+
+Robot::Robot(std::string name, std::string key) : _name(name), _key(key)
+    {_response=R"(
+      {
+        "model": "gpt-3.5-turbo",
+        "messages": [{"role": "user", "content": ""}],
+        "max_tokens": 4096,
+        "temperature": 0
+      }
+      )"_json;
+    }
+
+Robot::Robot(const Robot&){}
+Robot::~Robot(){}
+
+void Robot::setName(const std::string &name)
+{
+    this->_name = name;
+}
+
+const std::string &Robot::getName()
+{
+    return this->_name;
+}
+
+const std::string &Robot::getInput()
+{
+    return this->_input;
+}
+
+void Robot::ueserInput(const std::string& input)
+{
+    this->_input = input;
+    this->_response["messages"][0]["content"] = input;
+}
+
+void Robot::response()
+{
+    auto completion = openai::chat().create(_response);
+    std::cout<<_name<<':'<< completion.dump(2) <<std::endl;
+}
+
+void Robot::setTemperature(double temperature) 
+{
+    _response["temperature"] = temperature;
+}
